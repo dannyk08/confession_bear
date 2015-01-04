b35ec023a4e3ac7432a191217666b67f25f1c936
dromero08 <romerodaniel.k90@gmail.com>
2015-01-04 00:47:39 -0800

    Now stories cannot be less than 15 characters

--- confession_bear/app/models/confession.rb
+++ confession_bear/app/models/confession.rb
@@ -3,4 +3,7 @@ class Confession
   field :title, type: String
   field :story, type: String
   field :date_created, type: Date
+
+  validates :title, presence: true
+  validates :story, presence: true, length: { minimum: 15, maximum: 300 } 
 end
